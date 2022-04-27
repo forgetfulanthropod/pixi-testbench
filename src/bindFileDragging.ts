9e527840abac9432616d9aa194b8ae3022100fda
Charlie C Miles <charliecmiles@gmail.com>
2022-04-27 15:24:34 -0400

    spine almost working..

--- src/bindFileDragging.ts
+++ src/bindFileDragging.ts
@@ -4,6 +4,18 @@ import * as zip from "@zip.js/zip.js"
 // import { LoaderResource, Sprite } from "pixi.js"
 import * as PIXI from "pixi.js"
 import { manifest } from "./main"
+import { Application, TextureLoader } from "pixi.js"
+import { Spine, TextureAtlas } from "pixi-spine"
+import { SpineParser } from 'pixi-spine';
+import { AtlasAttachmentLoader, Skeleton, SkeletonData, SkeletonJson } from "@pixi-spine/runtime-4.0"
+// export {SpineParser};
+// export * from 'pixi-spine';
+
+
+
+// (new SpineParser()).createJsonParser()
+
+SpineParser.registerLoaderPlugin();
 
 export function bindFileDragNDrop(app: DemoApplication, container: HTMLElement, dragHover: HTMLDivElement) {
 
@@ -38,13 +50,13 @@ export function bindFileDragNDrop(app: DemoApplication, container: HTMLElement, 
         const isPng = file.type === "image/png"
 
         if (isPng) {
-            loadPng(file)
+            loadPng(file, app)
         }
 
         const isZip = file.type === "application/zip"
 
         if (isZip) {
-            loadZip(file)
+            loadZip(file, app)
         }
     }
 }
@@ -61,7 +73,7 @@ function getFile(e: DragEvent): File {
     return files[0]
 }
 
-async function loadPng(file: File) {
+async function loadPng(file: File, app: Application) {
     const url = URL.createObjectURL(file)
     
     const loadSprite = PIXI.Sprite.from(url)
@@ -69,13 +81,95 @@ async function loadPng(file: File) {
     app.stage.addChild(loadSprite)
 }
 
-async function loadZip(file: File) {
+async function loadZip(file: File, app: Application) {
     const entries = await getEntries(file as Blob, {})
     console.log({ entries })
+
+    const files: {name: string, url: string}[] = []
+    
+    await Promise.all(
+        entries.map(async entry => {
+            if (entry == null) return
+
+            const blobURL = await getURL(entry, 
+                // {
+                //     password: passwordInput.value,
+                //     onprogress: (index, max) => {
+                //         unzipProgress.value = index;
+                //         unzipProgress.max = max;
+                //     },
+                //     signal
+                // }
+            );
+
+            files.push({name: entry.filename, url: blobURL})
+        })
+    )
+
+    app.loader.add(files)
+    
+    // files.forEach(file => app.loader.add(file.name, file.url))
+
+    app.loader.load(async function (loader, resources) {
+        const jsonFile = files.find(file => file.name.includes('.json'))!
+        const atlasFile = files.find(file => file.name.includes('.atlas'))!
+        console.log(JSON.stringify({jsonFile, files}))
+        console.log({resource: resources?.[jsonFile.name]})
+        // const animation = new Spine(resources?.[jsonFile.name].spineData)
+        // console.log('parsed data', (new SpineParser).createJsonParser())
+        console.log('about to parse data')
+        console.log({
+            skeletonJson: await fetch(jsonFile.url).then(r => r.json()), 
+            atlasText: await fetch(atlasFile.url).then(r => r.text()), 
+        })
+        
+        new Skeleton(new SkeletonData())
+        const skel = new SkeletonJson(new AtlasAttachmentLoader(new TextureAtlas(
+            // atlasFile.url,
+            await fetch(atlasFile.url).then(r => r.text()),
+            (path, loaderFunction) => {
+                console.log({path,loaderFunction})
+            }
+        ))).readSkeletonData(await fetch(jsonFile.url).then(r => r.json()))
+
+
+        // console.log('parsed data', new TextureAtlas().addSpineAtlas(atlasFile.url))
+
+
+        // const animation = new Spine()
+        // const animation = new Spine((new SpineParser()).parseData(resources?.[jsonFile.name]).spineData)
+        // const animation = new Spine(new TextureAtlas(new File(atlasFile.url).stream()).)
+        // const animation = new Spine(skel)
+        // app.stage.addChild(animation)
+        // app.start()
+    })
+
+
+        // add the animation to the scene and render...
+    
+    // if (animation.state.hasAnimation('run')) {
+    //     // run forever, little boy!
+    //     animation.state.setAnimation(0, 'run', true);
+    //     // dont run too fast
+    //     animation.state.timeScale = 0.1;
+    // }
+    
+
+    // app.stage.addChild(spine)
 
     // app.loadStaged(file)
 }
 
-function getEntries(file: Blob, options: zip.ZipReaderGetEntriesOptions) {
+async function getURL(entry: zip.Entry, options = {}) {
+    return URL.createObjectURL(
+        //@ts-expect-error
+        await entry.getData(
+            new zip.BlobWriter(), 
+            options
+        )
+    )
+}
+
+function getEntries(file: Blob, options: zip.ZipReaderGetEntriesOptions): Promise<zip.Entry[]> {
     return new zip.ZipReader(new zip.BlobReader(file)).getEntries(options)
 }
